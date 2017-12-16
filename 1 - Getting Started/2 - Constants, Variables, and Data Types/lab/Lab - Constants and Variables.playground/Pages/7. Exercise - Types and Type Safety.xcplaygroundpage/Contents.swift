02a2388fd2f514384fce627566d2e4bd9964dfae
Yang, Yuxin <yyang@lcs-mba3025.lcs.on.ca>
2017-12-16 09:46:36 -0500

    All the labs through 1.1 to 1.4

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,21 +3,28 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
  */
-
+var firstDecimal = 0.0
+var secondDecimal = 0.0
 
 /*:
  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
+var trueOrFalse = true
+firstDecimal = trueOrFalse
+print("a bool value can not equal to a double")
 
 /*:
  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
+var name = "Jen"
+name = firstDecimal
+print("a string can not equal to double")
 
 /*:
  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
+var number = 10
+number = firstDecimal
+print("a integear can not equal to a double")
 
 //: [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
